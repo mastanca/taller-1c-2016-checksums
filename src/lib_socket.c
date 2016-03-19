7f645e2c36b27914aa37aa1c780c9009c117eb10
Martin Stancanelli <mastanca14@gmail.com>
2016-03-19 17:36:20 -0300

    Add listen, accept and connect

--- src/lib_socket.c
+++ src/lib_socket.c
@@ -10,6 +10,7 @@
 int socket_init(socket_t* skt){
 	int s = 0;
 	struct addrinfo hints;
+	const char *hostname = "localhost";
 	const char *serviceName = "12345";
 
 
@@ -18,16 +19,16 @@ int socket_init(socket_t* skt){
 	hints.ai_socktype = SOCK_STREAM; /* TCP  (or SOCK_DGRAM for UDP)    */
 	hints.ai_flags = 0;     	/* 0 (or AI_PASSIVE for server)           */
 
-	s = getaddrinfo(NULL, serviceName, &hints, &skt->result);
+	s = getaddrinfo(hostname, serviceName, &hints, &skt->result);
 
 	if (s != 0) {
-		printf("Error in getaddrinfo: %s\n", gai_strerror(s));
+		fprintf(stderr, "Error in getaddrinfo: %s\n", gai_strerror(s));
 		return 1;
 	}
 
 	skt->fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (skt->fd == -1){
-        printf("Error: %s\n", strerror(errno));
+		handle_error("init");
 		return 1;
 	}
 	return 0;
@@ -35,11 +36,11 @@ int socket_init(socket_t* skt){
 
 int socket_destroy(socket_t* skt){
 /*	if (shutdown(skt->fd, SHUT_RDWR) == -1){
-		fprintf(stderr, "There was an error when shuting down the socket: %s \n", strerror(errno));
+		handle_error("destroy (shutdown)");
 		return 1;
 	}*/
 	if (close(skt->fd) == -1){
-		fprintf(stderr, "Error closing file: %s\n", strerror(errno));
+		handle_error("destroy (close)");
 		return 1;
 	}
 	return 0;
@@ -47,12 +48,44 @@ int socket_destroy(socket_t* skt){
 
 int socket_bind(socket_t* skt){
 	if (bind(skt->fd, skt->result->ai_addr, skt->result->ai_addrlen ) == -1){
-		printf("Error: %s\n", strerror(errno));
+		handle_error("bind");
 		close(skt->fd);
 		freeaddrinfo(skt->result);
 		return 1;
 	}
 	freeaddrinfo(skt->result);
+	return 0;
+}
+
+int socket_listen(socket_t* skt, int max_clients){
+	if (listen(skt->fd, max_clients) == -1){
+		handle_error("listen");
+		return 1;
+	}
+	return 0;
+}
+
+int socket_accept(socket_t* skt, socket_t* client_skt){
+	client_skt->fd = accept(skt->fd, NULL, NULL);
+	if (client_skt->fd == -1){
+		handle_error("accept");
+		return 1;
+	}
+	return 0;
+}
+
+int socket_connect(socket_t* skt){
+	if (connect(skt->fd, skt->result->ai_addr, skt->result->ai_addrlen) == -1){
+		handle_error("connect");
+		close(skt->fd);
+		return 1;
+	}
+	return 0;
+}
+
+int handle_error(char* function_name){
+	fprintf(stderr, "Error on %s: ", function_name);
+	fprintf(stderr, "%s\n", strerror(errno));
 	return 0;
 }
 
--- src/main.c
+++ src/main.c
@@ -13,12 +13,36 @@
 
 int main(int argc, char *argv[])
 {
-	socket_t skt;
-	socket_init(&skt);
-	printf("%s \n", "Socket created!");
-	socket_bind(&skt);
-	printf("%s \n", "Socket binded!");
-	socket_destroy(&skt);
-	printf("%s \n", "Socket destroyed!");
-	return 0;
+	char* execution_type = argv[1];
+	if (strcmp(execution_type, "client") == 0){
+		socket_t skt;
+		socket_init(&skt);
+		printf("%s \n", "Socket created!");
+		socket_connect(&skt);
+		printf("%s \n", "Socket connected!");
+		socket_destroy(&skt);
+		printf("%s \n", "Socket destroyed!");
+		return 0;
+
+	}else if(strcmp(execution_type, "server") == 0){
+		socket_t acep;
+		socket_init(&acep);
+		printf("%s \n", "Socket created!");
+		socket_bind(&acep);
+		printf("%s \n", "Socket binded!");
+		socket_listen(&acep, 5);
+		printf("%s \n", "Socket listening!");
+		socket_t client_skt;
+		socket_accept(&acep, &client_skt);
+		printf("%s \n", "Client accepted!");
+		socket_destroy(&client_skt);
+		printf("%s \n", "Client destroyed!");
+		socket_destroy(&acep);
+		printf("%s \n", "Socket destroyed!");
+		return 0;
+	}
+
+
+
+
 }
