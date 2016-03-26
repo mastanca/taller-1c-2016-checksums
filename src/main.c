6d24afeeab0a94af4ce1a280ac0fa41cf16ad17d
Martin Stancanelli <mastanca14@gmail.com>
2016-03-26 02:05:53 -0300

    Remove whitespaces

--- src/main.c
+++ src/main.c
@@ -8,15 +8,12 @@
 
 #include <string.h>
 #include "client.h"
-//#include "lib_checksum.h"
 #include "server.h"
 
 #define CLIENT "client"
 #define SERVER "server"
 
-
 int main(int argc, char *argv[]){
-// SERVER TESTING
 	char* execution_type = argv[1];
 	if (strcmp(execution_type, CLIENT) == 0){
 		return client_execution(argc, argv);
