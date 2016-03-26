433f13cd4618f304cfa60237767a185a714ff9e5
Martin Stancanelli <mastanca14@gmail.com>
2016-03-26 15:03:21 -0300

    Compile with makefile ok

--- src/file_handler.c
+++ src/file_handler.c
@@ -5,20 +5,18 @@
  *      Author: mastanca
  */
 
-#include "file_handler.h"
-#include <stdlib.h>
+ #include "file_handler.h"
+ #include <stdlib.h>
 
-// Reads block_size chars from file and return result in buffer
-int read_from_file(FILE* file, char* buffer, size_t block_size){
-	char tmp_buffer[block_size + 1];
-	memset(tmp_buffer, 0 , sizeof(tmp_buffer));
-	if (!feof(file)){
-//		fgets(tmp_buffer, block_size + 1, file);
-		fread(tmp_buffer, 1, block_size, file);
-		if (strlen(tmp_buffer) == block_size)
-			strcpy(buffer, tmp_buffer);
-	}
-	return EXIT_SUCCESS;
-}
-
-
+ // Reads block_size chars from file and return result in buffer
+ int read_from_file(FILE* file, char* buffer, size_t block_size){
+ 	char tmp_buffer[block_size + 1];
+ 	memset(tmp_buffer, 0 , sizeof(tmp_buffer));
+ 	if (!feof(file)){
+ 		if (fread(tmp_buffer, 1, block_size, file) != 0){
+ 			if (strlen(tmp_buffer) == block_size)
+ 				strncpy(buffer, tmp_buffer, sizeof(tmp_buffer));
+ 		}
+ 	}
+ 	return EXIT_SUCCESS;
+ }
